8b92fe7f96beb3bcd93319197df86ebaff9c1bf0
Murari Kumar <murarikumar42@gmail.com>
2023-03-12 07:38:38 +0530

    75. Sort Colors leetcode

---
+++ src/main/java/murari/striver/sdesheet/arrays/SortColors.java
@@ -0,0 +1,43 @@
+package murari.striver.sdesheet.arrays;
+
+import java.util.Arrays;
+
+public class SortColors {
+    public  void sortColors(int[] nums) {
+        int low=0;
+        int high=nums.length-1;
+        int mid=0;
+        while(mid<=high){
+            switch (nums[mid]){
+                case 0:{
+                    swap(nums,mid,low);
+                    low++;
+                    mid++;
+                    break;
+                }
+                case 1:{
+                    mid++;
+                    break;
+                }
+                case 2:{
+                    swap(nums,mid,high);
+                    high--;
+                    break;
+                }
+            }
+        }
+    }
+    public void swap(int [] arr,int indx1,int indx2){
+        int temp=arr[indx1];
+        arr[indx1]=arr[indx2];
+        arr[indx2]=temp;
+    }
+
+    public static void main(String[] args) {
+//        int [] list=new int[]{2,0,2,1,1,0};
+        int [] list=new int[]{0,1,0};
+        SortColors obj1=new SortColors();
+        obj1.sortColors(list);
+        Arrays.stream(list).forEach(System.out::println);
+    }
+}
