eeb6d3ee8baf3efe8570ce7ec379ae80692c4ca0
Murari Kumar <murarikumar42@gmail.com>
2023-03-18 09:54:05 +0530

    128. Longest Consecutive Sequence leetcode

---
+++ src/main/java/murari/striver/sdesheet/arrayspart4/LongestConsecutive.java
@@ -0,0 +1,28 @@
+package murari.striver.sdesheet.arrayspart4;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class LongestConsecutive {
+    public int longestConsecutive(int[] nums) {
+        Set<Integer> set=new HashSet<>();
+        for(Integer num:nums){
+            set.add(num);
+        }
+        int longestStreak=0;
+        for(Integer num: nums){
+            if(!set.contains(num-1)){
+                int currentNum=num;
+                int currentStreak=1;
+
+                while (set.contains(currentNum+1)){
+                    currentStreak++;
+                    currentNum++;
+                }
+                longestStreak=Math.max(longestStreak,currentStreak);
+            }
+        }
+        return longestStreak;
+    }
+
+}
