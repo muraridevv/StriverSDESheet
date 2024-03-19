100d8c44311bc7bcd92c38c3a769fd9495806899
Murari Kumar <murarikumar42@gmail.com>
2024-03-19 10:11:28 +0530

    19. Remove Nth Node from End of Linked List

---
+++ src/main/java/murari/striver/sdesheet/linkedlist/RemoveNthNodeFromEnd.java
@@ -0,0 +1,26 @@
+package murari.striver.sdesheet.linkedlist;
+
+public class RemoveNthNodeFromEnd {
+
+    public ListNode removeNthFromEnd(ListNode head, int n) {
+        ListNode slowNode= head;
+        ListNode fastNode = head;
+
+        // move the fastNode to n nodes ahead
+        for(int i=0;i<n;i++){
+            fastNode=fastNode.next;
+        }
+
+        // If fastNode becomes null, the Nth node from the end is the head
+        if(fastNode.next==null){
+            return head.next;
+        }
+        // move both nodes simultaneously until fastNode reaches the end
+        while (fastNode!=null){
+            slowNode=slowNode.next;
+            fastNode=fastNode.next;
+        }
+        slowNode.next=slowNode.next.next;
+        return head;
+    }
+}
