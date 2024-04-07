27cb0801f692f84b1f4902eb1ea7d49120fd0de2
Murari Kumar <murarikumar42@gmail.com>
2024-04-07 08:39:20 +0530

    FEAT: SubSetSum1 using recursion

--- src/main/java/murari/striver/sdesheet/linkedlist/ReverseList.java
+++ src/main/java/murari/striver/sdesheet/linkedlist/ReverseList.java
@@ -2,13 +2,7 @@ package murari.striver.sdesheet.linkedlist;
 
 
 //  Definition for singly-linked list.
-class ListNode {
-    int val;
-    ListNode next;
-    ListNode() {}
-    ListNode(int val) { this.val = val; }
-    ListNode(int val, ListNode next) { this.val = val; this.next = next; }
-}
+
 public class ReverseList {
     public ListNode reverseListIterative(ListNode head) {
         ListNode previous=null;
