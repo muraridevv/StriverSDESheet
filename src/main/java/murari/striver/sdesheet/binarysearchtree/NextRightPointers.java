efff7c120538e3bb643049a09a6572f349b8399b
Murari Kumar <murarikumar42@gmail.com>
2024-04-24 05:00:53 +0530

    FEAT: Binary Search Tree Problems

---
+++ src/main/java/murari/striver/sdesheet/binarysearchtree/NextRightPointers.java
@@ -0,0 +1,26 @@
+package murari.striver.sdesheet.binarysearchtree;
+
+public class NextRightPointers {
+
+    public Node connect(Node root) {
+        connectHelper(root,0);
+        return root;
+    }
+
+    private void connectHelper(Node node, int level){
+        if(node==null) return;
+
+        if(node.left!=null){
+            node.left.next=node.right;
+        }
+
+        if(node.right!=null && node.next!=null){
+            node.right.next=node.next.left;
+        }
+
+        connectHelper(node.left, level+1);
+        connectHelper(node.right, level+1);
+
+
+    }
+}
---
+++ src/main/java/murari/striver/sdesheet/binarysearchtree/ValidateBST.java
@@ -0,0 +1,22 @@
+package murari.striver.sdesheet.binarysearchtree;
+
+public class ValidateBST {
+    public boolean isValidBST(TreeNode root) {
+        return isValid(root, Long.MIN_VALUE, Long.MAX_VALUE);
+    }
+
+    boolean isValid(TreeNode root, long min, long max) {
+
+        if(root==null) return true;
+
+        if(root.val<=min || root.val>=max) return false;
+
+        // return isValid(root.left, min, root.val)&& isValid(root.right, root.val, max);
+        boolean left = isValid(root.left, min, root.val);
+        boolean right = isValid(root.right, root.val, max);
+
+        if (left && right) return true;
+
+        return false;
+    }
+}
