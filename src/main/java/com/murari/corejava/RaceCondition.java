2a32c7ec140fb1ee32ba19101b56df58c70b687b
Murari Kumar <murarikumar42@gmail.com>
2024-05-14 04:43:37 +0530

    FEAT: java lambdas comparison

---
+++ src/main/java/com/murari/corejava/RaceCondition.java
@@ -0,0 +1,33 @@
+package com.murari.corejava;
+
+public class RaceCondition {
+
+    public static void main(String[] args) {
+
+    }
+}
+
+class IncrementerThread extends Thread{
+    private int iterations;
+    private volatile int x;
+
+    private IncrementerThread(int iterations){
+        this.iterations= iterations;
+    }
+
+    @Override
+    public void run() {
+       for(int i=0; i< iterations; i++){
+           int temp=x;
+           temp++;
+           x=temp;
+       }
+
+
+
+    }
+
+    public int getX() {
+        return x;
+    }
+}
---
+++ src/main/java/com/murari/corejava/synchronization/SynchronizedExample.java
@@ -0,0 +1,45 @@
+package com.murari.corejava.synchronization;
+
+public class SynchronizedExample {
+
+    private int count=0;
+
+    public  void increment(){
+        count++;
+    }
+
+    public  void decrement(){
+        count--;
+    }
+
+    public synchronized int getCount(){
+        return count;
+    }
+
+    public static void main(String[] args) {
+        SynchronizedExample synchronizedExample= new SynchronizedExample();
+
+        Thread incrementThread= new Thread(()->{
+           for(int i=0;i< 10000; i++){
+               synchronizedExample.increment();
+           }
+        });
+
+        Thread decrementThread= new Thread(()->{
+            for(int i=0;i< 10000; i++){
+                synchronizedExample.decrement();
+            }
+        });
+
+        incrementThread.start();
+        decrementThread.start();
+
+        try {
+            incrementThread.join();
+            decrementThread.join();
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+        System.out.println("Final count"+ synchronizedExample.count);
+    }
+}
