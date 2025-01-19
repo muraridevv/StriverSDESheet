bb62a78fe7329e780e4803d8d51628d5a4ba6652
Murari Kumar <murarikumar42@gmail.com>
2025-01-19 09:07:31 +0530

    FEAT: CanPlaceFlowers

---
+++ src/main/java/com/murari/contest/CanPlaceFlowers.java
@@ -0,0 +1,26 @@
+package com.murari.contest;
+//https://leetcode.com/problems/can-place-flowers/?envType=study-plan-v2&envId=leetcode-75
+public class CanPlaceFlowers {
+    public boolean canPlaceFlowers(int[] flowerbed, int n) {
+        int count=0;
+        for(int i=0; i< flowerbed.length;i++){
+            boolean leftAllowed= i==0 || flowerbed[i-1]==0;
+            boolean rightAllowed= i==flowerbed.length-1 || flowerbed[i+1]==0;
+
+            if(leftAllowed && rightAllowed && flowerbed[i]==0){
+                flowerbed[i]=1;
+                count++;
+            }
+        }
+        return count>=n;
+    }
+
+    public static void main(String[] args) {
+        CanPlaceFlowers obj= new CanPlaceFlowers();
+//        int[] flowerbed = new int[]{1,0,0,0,1}; length=5,count+n=3
+//        int[] flowerbed = new int[]{1,0,0,0,1,0};
+        int[] flowerbed = new int[]{0,1,0};
+        int n=1;
+        System.out.println(obj.canPlaceFlowers(flowerbed,n));
+    }
+}
