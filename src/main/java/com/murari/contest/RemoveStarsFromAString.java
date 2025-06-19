7e7ca21b1f891bc3bda50519fd702758df269ecb
Murari Kumar <murarikumar42@gmail.com>
2025-06-19 10:04:46 +0530

    FEAT: Remove Stars from a string and Max Coin Change

---
+++ src/main/java/com/murari/contest/RemoveStarsFromAString.java
@@ -0,0 +1,24 @@
+package com.murari.contest;
+
+public class RemoveStarsFromAString {
+
+    public String removeStars(String s) {
+        StringBuilder result= new StringBuilder();
+
+        for(Character ch: s.toCharArray()){
+
+            if(ch == '*'){
+                if(!result.isEmpty())
+                    result.deleteCharAt(result.length()-1);
+            }
+            else result.append(ch);
+        }
+        return result.toString();
+    }
+
+    public static void main(String[] args) {
+        RemoveStarsFromAString obj= new RemoveStarsFromAString();
+        String s = "leet**cod*e";
+        System.out.println(obj.removeStars(s));
+    }
+}
